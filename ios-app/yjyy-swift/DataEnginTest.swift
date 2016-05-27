30e809cf1d14488e48b1ac8572a76f7a62d48234
chai2010 <chaishushan@gmail.com>
2016-05-27 15:14:05 +0800

    ios-app/yjyy-swift: 引入 XCTest 测试框架

---
+++ ios-app/yjyy-swift/DataEnginTest.swift
@@ -0,0 +1,28 @@
+// Copyright 2016 <chaishushan{AT}gmail.com>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+import XCTest
+
+@testable import yjyy
+
+class DataEnginTest: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+	// 查询的性能
+	func testBenchSearch() {
+		self.measureBlock {
+			var db = DataEngin()
+			let result = db.SearchV2("")
+			_ = result
+		}
+	}
+
+}
